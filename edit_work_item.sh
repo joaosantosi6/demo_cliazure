d8a41093ec58605f7ff943a6f97d903f659b897c
joaosantosi6 <joao.santos@infinity6.ai>
2024-07-04 10:16:46 -0300

    change state

---
+++ edit_work_item.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# sudo apt-get install azure-cli
+# az extension add --name azure-devops
+# az login
+
+PAT=$AZURE_DEVOPS_PAT
+
+export organization=joaosantos0209
+export project=Teste_cli
+ORGANIZATION_URL="https://dev.azure.com/${organization}"
+
+echo $PAT | az devops login --organization $ORGANIZATION_URL
+
+az devops configure --defaults organization=https://dev.azure.com/${organization} project=${project}
+
+# ID do work item a ser atualizado
+WORK_ITEM_ID=1
+
+# Atualizar o campo "State" para "Done"
+az boards work-item update --id $WORK_ITEM_ID --fields "System.State=Done"
+
+echo "Work item $WORK_ITEM_ID atualizado com sucesso."
